d288a54ceb38ce2c47704d8968a2e8bbe1f31255
prajyot.kerkar <prajyot.kerkar@accenture.com>
2026-07-02 19:45:41 +0530

    error corrected

--- amzconnect-supporting-infra/terraform/data.tf
+++ amzconnect-supporting-infra/terraform/data.tf
@@ -1,7 +1,3 @@
-data "aws_ssm_parameter" "amz-connect-instance-id" {
-  name = "/${var.env}/${local.region_shortnames[var.region]}/${var.ivr_id}/amz-connect-instance-id"
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
